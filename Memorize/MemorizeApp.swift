7f675cc730ecdcb7bb490a16c67be91130f03cab
Dallas Carraher <dallas.carraher@gmail.com>
2020-09-13 12:15:21 -0700

    Initial Project Upload
    Initial commit adding project files as well as the first lecture code.

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Dallas Carraher on 9/12/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
